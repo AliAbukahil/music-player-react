f797fa13982d0d4becd01f7deec73e9d245395e9
ali.abukahil <ali.abukahil@gmail.com>
2021-07-24 21:06:39 +0200

    14. Selecting Songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,15 +1,23 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 // Importing Components
 import LibrarySong from "./LibrarySong";
 
-function Library({ songs }) {
+function Library({ songs, setCurrentSong }) {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            song={song}
+            id={song.id}
+            key={song.id}
+            // audioRef={audioRef}
+            // isplaying={isPlaying}
+            // setSongs={setSongs}
+          />
         ))}
       </div>
     </div>
